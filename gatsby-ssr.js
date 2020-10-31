b0faaafeb79b1ee65bfb784ed2db46ad3dde1a37
kakitaku00 <webtrakakinuma@gmail.com>
2020-10-31 11:36:03 +0900

    fix: gatsby-ssrにstyleタグをsortする関数を追加

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+exports.onPreRenderHTML = function onPreRenderHTML({
+  getHeadComponents,
+  replaceHeadComponents,
+}) {
+  const headComponents = getHeadComponents();
+  headComponents.sort((a, b) => {
+    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+      return 0;
+    }
+
+    if (a.type === 'style') {
+      return 1;
+    } else if (b.type === 'style') {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  replaceHeadComponents(headComponents);
+};
