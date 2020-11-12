2181b77d0ebc19d5bbab5e11d47bf42a4e1f3b0c
kakitaku00 <webtrakakinuma@gmail.com>
2020-11-12 19:14:11 +0900

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = ({ siteTitle, tag }) => {
   const Logo = tag
   return (
     <header className="header bg-teal-500">
-      <div className="max-w-screen-xl mx-auto xl:px-24 lg:px-16 md:px-12 sm:px-8 px-3 py-4 md:py-6 flex items-center lg:justify-start justify-center">
+      <div className="max-w-screen-xl mx-auto xl:px-16 md:px-12 sm:px-8 px-3 py-4 md:py-6 flex items-center lg:justify-start justify-center">
         <Logo className="m-0">
           <Link to="/" className="text-white text-xl md:text-2xl font-bold">
             {siteTitle}
