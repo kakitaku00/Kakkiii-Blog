d1d993111a5ef070d16ab70c13a1b4994071b514
kakitaku00 <webtrakakinuma@gmail.com>
2021-09-15 19:04:22 +0900

    update: fontawsome style fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+import "@fortawesome/fontawesome-svg-core/styles.css"
 import "prismjs/themes/prism-okaidia.css"
 import "./src/styles/layout.css"
 import "./src/styles/tailwind.css"
 import "./src/styles/markdown.css"
+
+import { config } from "@fortawesome/fontawesome-svg-core"
+
+config.autoAddCss = false
